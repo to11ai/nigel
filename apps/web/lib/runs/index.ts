e7874e3d074047d44b95c005afe448a6552d199e
Matt Calhoun <matt@mattcalhoun.com>
2026-05-08 17:21:48 -0400

    feat(runs): add barrel export

---
+++ apps/web/lib/runs/index.ts
@@ -0,0 +1,33 @@
+export { Run, type CreateRunInput } from "./create";
+
+export {
+  assertValidTransition,
+  isValidTransition,
+  type RunStatus,
+  terminalStates,
+} from "./state-machine";
+
+export {
+  type AgentRun,
+  DEFAULT_MAX_CHILDREN,
+  MAX_DEPTH,
+  type SandboxPolicy,
+  sandboxPolicySchema,
+  type TriggerSource,
+  triggerSourceSchema,
+  runStatusSchema,
+} from "./types";
+
+export {
+  addCostMicros,
+  getRun,
+  insertRun,
+  listChildren,
+  updateRunStatus,
+} from "./repository";
+
+export { computeCostMicros, PRICING, type TokenUsage } from "./cost";
+
+export { onRunStatusChange } from "./lifecycle";
+
+export { isRunsEnabled } from "./feature-flag";
